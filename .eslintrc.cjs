5c0616ced2e9488a25dd42e88da1d4193f0eeddd
Eugene Solomonik <solosoft2000@mail.ru>
2024-02-08 19:57:26 +0200

    chore: tsconfig, eslint, prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'error',
+  },
+};
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+console.log("'CRUD API Start'");
+{
+  console.log('in block');
+}
+
+const a = (asd: number, bsd: number): void => {
+    console.log(asd + bsd );
+    
+}
+
+a(2, 4);

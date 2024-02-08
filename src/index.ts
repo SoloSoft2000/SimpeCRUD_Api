ed68fe5f0a522680e7678f6cf7c740d47040eecb
Eugene Solomonik <solosoft2000@mail.ru>
2024-02-08 21:27:04 +0200

    chore: config

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import http from 'http';
 http
   .createServer(function (_, response) {
-    response.end('Hello Node JS!');
+    response.end('Hello Node.JS!');
   })
   .listen(3000, '127.0.0.1', function () {
     console.log(' Server listen port 3000');

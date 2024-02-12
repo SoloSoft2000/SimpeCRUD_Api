4be990c97b2e4b7cc01ae87c1e57259d9194bb6b
Eugene Solomonyk <solosoft2000@mail.ru>
2024-02-12 17:51:45 +0200

    feat: start with routes in worker

--- src/cluster/workerServer.ts
+++ src/cluster/workerServer.ts
@@ -3,20 +3,27 @@ import { resId } from '../utils/generateId';
 
 export const workerServer = (responses): http.Server => {
   return http.createServer((req, res) => {
-    const currRes = resId();
-    responses[currRes] = res;
-    if (req.method === 'GET') {
-      process.send({ reqToBD: true, action: 'getUsers', responseId: currRes });
+    try {
+      const currRes = resId();
+      responses[currRes] = res;
+      if (req.method === 'GET' && req.url === '/api/users') {
+        process.send({ reqToBD: true, action: 'getUsers', responseId: currRes });
+      } else if (req.method === 'POST' && req.url === '/api/users') {
+        let requestBody = '';
+        req.on('data', (chunk) => {
+          requestBody += chunk;
+        });
+        req.on('end', () => {
+          process.send({ reqToBD: true, action: 'createUser', responseId: currRes, userData: requestBody });
+        });
+      } else {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: "Endpoint doesn't exist" }));
+      }
+      console.log(`worker ${process.pid} received request`);
+    } catch (error) {
+      res.writeHead(500, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: 'Internal Server Error' }));
     }
-    if (req.method === 'POST') {
-      let requestBody = '';
-      req.on('data', (chunk) => {
-        requestBody += chunk;
-      });
-      req.on('end', () => {
-        process.send({ reqToBD: true, action: 'createUser', responseId: currRes, userData: requestBody });
-      });
-    }
-    console.log(`worker ${process.pid} received request`);
   });
 };
